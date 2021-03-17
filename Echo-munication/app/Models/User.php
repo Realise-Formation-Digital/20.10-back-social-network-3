3a4b235bfafe82b140c7964e6db4f45aa32114ed
Chris <christophermoret@gmail.com>
2021-03-17 12:24:02 +0100

    Merge with Master

--- Echo-munication/app/Models/User.php
+++ Echo-munication/app/Models/User.php
@@ -2,46 +2,42 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use HasFactory, Notifiable;
 
-    use HasFactory;
-
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name',
-        'username',
         'email',
-        'avatar',
-        'password'
-    ];
-
-    protected $hidden = [
-        'created_at',
-        'updated_at',
-        'remember_token',
-        'password'
+        'password',
     ];
 
     /**
-     * Relations
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
      */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
-    public function tutorials()
-    {
-        return $this->hasMany(tutorial::class)->withCount('comments', 'likes');
-    }
-
-    public function likes()
-    {
-        return $this->hasMany(Like::class);
-    }
-
-    public function comments()
-    {
-        return $this->hasMany(Comment::class);
-    }
-    
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
