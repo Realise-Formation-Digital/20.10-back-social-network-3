39537ab0ff4a3a48cbf25902a060b1c70e431453
Simon Zanotelli <zanotellisimon@gmail.com>
2021-03-18 11:14:25 +0100

    Squelette des structure front-end

---
+++ Echo-munication/resources/js/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<Header />
+
+		<div id="wrapper">
+			<Sidebar />
+
+			<div id="content-wrapper">
+				<router-view></router-view> <!-- container-fluid div Replaced by router-view -->
+				
+
+				<Footer />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Header from "./components/layout/Header.vue";
+import Sidebar from "./components/layout/Sidebar.vue";
+import Footer from "./components/layout/Footer.vue";
+
+export default {
+	components: {
+		Header,
+		Sidebar,
+		Footer
+	},
+	created() {
+		document.querySelector("body").style.backgroundColor = "#fff";
+	}
+};
+</script>
+};
+</script>
---
+++ Echo-munication/resources/js/components/layout/Footer.vue
@@ -0,0 +1,10 @@
+<!-- Footer.vue -->
+<template>
+    <footer class="sticky-footer">
+        <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+                <span>Copyright © Echo-munication 2021 @ realise</span>
+            </div>
+        </div>
+    </footer>
+</template>
